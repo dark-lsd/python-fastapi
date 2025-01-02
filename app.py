1b070cc068a57515219d1c990c2f3772dc727b83
dark-lsd <hallucinationslsd@gmail.com>
2025-01-02 17:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
@@ -10,5 +9,3 @@ def index():
 @app.get("/test")
 def test():
     return "Test"
-
-uvicorn.run(app, host="0.0.0.0", port=8080)
