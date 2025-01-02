68c305ca691eff82b8486aa19971a8eb271b3ef4
dark-lsd <hallucinationslsd@gmail.com>
2025-01-02 12:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from fastapi import FastAPI
-import uvicorn
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/test_route")
-def read_test_route():
-    return {"Hello": "Test Route"}
-
-@app.get("/route2")
-def read_route():
+def index():
     return "Hello world"
 
-uvicorn.run(app, host="0.0.0.0", port=8080)
+@app.get("/test")
+def test():
+    return "Test"
